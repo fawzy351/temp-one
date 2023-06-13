025f791f9cc32b6bf59c18f30f8e7589d601227c
fawzyebrahim351 <fawzyebrahim777@gmail.com>
2023-06-13 18:09:41 +0200

    add up button

--- javascript/main.js
+++ javascript/main.js
@@ -10,3 +10,22 @@ icon.onclick = function(){
     span.classList.toggle('width');
     list.classList.toggle('navBar')
 }
+// up button 
+let btn = document.getElementById('up');
+
+window.onscroll = function(){
+    if(scrollY >= 400){
+        btn.style.bottom = '20px'
+    }else{
+        btn.style.bottom = '-40px'
+    }
+}
+btn.onclick = function(){
+    scroll(0,0)
+}
+btn.onmousedown = function(){
+    btn.style.opacity = '30%'
+}
+btn.onmouseup = function(){
+    btn.style.opacity = '100%'
+}
